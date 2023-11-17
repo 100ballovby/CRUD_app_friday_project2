999cc97f6bc4201996c2654a9559f0f8e96324e6
greatraksin <greatraksin@icloud.com>
2023-11-17 18:42:18 +0300

    Initial commit

---
+++ routes.py
@@ -0,0 +1,19 @@
+from app import app
+from flask import render_template, request, url_for
+
+
+@app.route('/')
+def index():  # функция для главной страницы (она ее отображает)
+    user = {'username': 'GreatRaksin'}
+    return render_template('index.html', title='Главная страница', user=user)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='О нас')
+
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contact.html', title='Контакты')
+
