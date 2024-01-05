517193def3a6e828f85d1fd6a1f144c06692beeb
greatraksin <greatraksin@icloud.com>
2024-01-05 15:29:40 +0300

    finishing companies pages

--- forms.py
+++ forms.py
@@ -1,18 +1,31 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, EmailField, SelectField, SubmitField
+from wtforms import StringField, EmailField, SelectField, SubmitField, widgets
 from wtforms.validators import DataRequired
 from models import Company
 
 
 class UserForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    username = StringField('Username', validators=[DataRequired()])
-    email = EmailField('Email', validators=[DataRequired()])
-    street = StringField('Street', validators=[DataRequired()])
-    city = StringField('City', validators=[DataRequired()])
-    phone = StringField('Phone', validators=[DataRequired()])
-    company_id = SelectField('Company', coerce=int)
-    submit = SubmitField('Add')
+    name = StringField('Name', validators=[DataRequired()],
+                       render_kw={'class': 'form-control my-3',
+                                  'placeholder': 'Name'})
+    username = StringField('Username', validators=[DataRequired()],
+                           render_kw={'class': 'form-control my-3',
+                                      'placeholder': 'User Name'})
+    email = EmailField('Email', validators=[DataRequired()],
+                       render_kw={'class': 'form-control my-3',
+                                  'placeholder': 'example@example.com'})
+    street = StringField('Street', validators=[DataRequired()],
+                         render_kw={'class': 'form-control my-3',
+                                    'placeholder': 'Street'})
+    city = StringField('City', validators=[DataRequired()],
+                       render_kw={'class': 'form-control my-3',
+                                  'placeholder': 'City'})
+    phone = StringField('Phone', validators=[DataRequired()],
+                        render_kw={'class': 'form-control my-3',
+                                   'placeholder': '+375291234567'})
+    company_id = SelectField('Company', coerce=int,
+                             render_kw={'class': 'form-select'})
+    submit = SubmitField('Add', render_kw={'class': 'btn btn-primary mt-3'})
 
     def __init__(self):
         super().__init__()
@@ -23,6 +36,9 @@ class UserForm(FlaskForm):
 
 
 class CompanyForm(FlaskForm):
-    name = StringField('Company Name', validators=[DataRequired()])
-    website = StringField('Website', validators=[DataRequired()])
-
+    name = StringField('Company Name', validators=[DataRequired()],
+                       render_kw={'class': 'form-control my-3',
+                                  'placeholder': 'Company name'})
+    website = StringField('Website', validators=[DataRequired()],
+                          render_kw={'class': 'form-control my-3', 'placeholder': 'Website'})
+    submit = SubmitField('Add', render_kw={'class': 'btn btn-primary mt-3'})
